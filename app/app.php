76b08697a5a17c9473b3bea94a53282df9af7d1b
Annie Hoogendoorn & Jose Torres <student@epicodus.com>
2015-08-06 10:51:43 -0700

    add views files and update root route for home page

--- app/app.php
+++ app/app.php
@@ -10,30 +10,8 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() {
-      return "<!DOCTYPE html>
-      <html>
-      <head>
-          <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-          <title>Find a Car</title>
-      </head>
-      <body>
-          <div class='container'>
-              <h1>Find a Car!</h1>
-              <form action='/car'>
-                  <div class='form-group'>
-                      <label for='price'>Enter Maximum Price:</label>
-                      <input id='price' name='price' class='form-control' type='number'>
-                  </div>
-                  <div class='form-group'>
-                      <label for='miles'>Enter Maximum Mileage:</label>
-                      <input id='miles' name='miles' class='form-control' type='number'>
-                  </div>
-                  <button type='submit' class='btn-success'>Submit</button>
-              </form>
-          </div>
-      </body>
-      </html>";
+    $app->get("/", function() use ($app) {
+      return $app['twig']->render('dealership_search.html.twig', array('cars_matching_search' => Car::getAll()));
 
     });
 
