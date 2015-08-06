482a1af1c981c7e8a08f44f77e9a3d291715ebe4
Annie Hoogendoorn & Jose Torres <student@epicodus.com>
2015-08-06 14:50:09 -0700

    fix url path issues.

--- app/app.php
+++ app/app.php
@@ -20,7 +20,7 @@
     });
 
 
-      $app->post("/search_results", function() {
+      $app->get("/search_results", function() {
       $first_car = new Car("2014 Porsche 911", 7864, 114991, "img/porsche.jpg");
       $second_car = new Car("2011 Ford F450", 14000, 55995, "img/ford.jpeg");
       $third_car = new Car("2013 Lexus RX 350", 20000, 44700, "img/lexus.jpg");
