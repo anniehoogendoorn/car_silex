1b1735b8cc7954d0f314ad2f762f9e765c389366
Annie Hoogendoorn & Jose Torres <student@epicodus.com>
2015-08-06 10:28:54 -0700

    Update readme.md, update composer.json to include twig, update app.php

--- app/app.php
+++ app/app.php
@@ -2,9 +2,13 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/car.php";
 
-    $app['debug'] = true;
+    session_start();
 
     $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
 
     $app->get("/", function() {
       return "<!DOCTYPE html>
